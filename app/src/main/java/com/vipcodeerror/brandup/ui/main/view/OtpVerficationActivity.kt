06f8cf8d429b5b2535346e17982cb6f235874017
123.vipulj@gmail.com <viPUl123jurel>
2021-03-03 14:23:32 +0530

    otp layout design changes

---
+++ app/src/main/java/com/vipcodeerror/brandup/ui/main/view/OtpVerficationActivity.kt
@@ -0,0 +1,28 @@
+package com.vipcodeerror.brandup.ui.main.view
+
+import `in`.aabhasjindal.otptextview.OTPListener
+import `in`.aabhasjindal.otptextview.OtpTextView
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.vipcodeerror.brandup.R
+
+class OtpVerficationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.otp_layout);
+
+        val otpView = findViewById<OtpTextView>(R.id.otp_view);
+        otpView.otpListener = object: OTPListener {
+            override fun onInteractionListener() {
+
+            }
+
+            override fun onOTPComplete(otp: String) {
+
+            }
+
+        }
+
+    }
+}
