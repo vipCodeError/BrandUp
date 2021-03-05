c86b4df5ecf86b205bfb36f7d4b29006fa5d6657
123.vipulj@gmail.com <viPUl123jurel>
2021-03-05 18:36:03 +0530

    setting layout done

---
+++ app/src/main/java/com/vipcodeerror/brandup/ui/main/view/Settings.kt
@@ -0,0 +1,17 @@
+package com.vipcodeerror.brandup.ui.main.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.vipcodeerror.brandup.R
+
+class Settings : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = inflater.inflate(R.layout.fragment_settings, container, false)
+        return view
+    }
+}
